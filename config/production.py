4c88101fb4ebbd9701f29a5b05661c602e8086d2
grigory <grigory.timofeev@saritasa.com>
2021-04-04 00:11:11 +0700

    Add first version of docker-compose file

--- config/production.py
+++ config/production.py
@@ -7,9 +7,7 @@ SITE_ID = 1
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5_wteua^80be^ku_3h2#n33)i%2@)uu*lamy4(xsxb4^n*piy!'
 
-ALLOWED_HOSTS = [
-    '130.193.54.188',
-]
+ALLOWED_HOSTS = ['*']
 
 ROOT_URLCONF = 'config.urls'
 
