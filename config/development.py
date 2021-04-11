b3bedc95074a5ce432c09116692eb68fbcbf8f4e
grigory <grigory.timofeev@saritasa.com>
2021-04-11 10:28:55 +0700

    Add Nginx and prepare prod environment
    Also simplify Dockerfile and add missing settings

--- config/development.py
+++ config/development.py
@@ -1,6 +1,8 @@
 from .common import *
 
 DEBUG = True
+if 'celery' in sys.argv[0]:
+    DEBUG = False
 
 INSTALLED_APPS += [
     'debug_toolbar',
@@ -10,7 +12,6 @@ MIDDLEWARE += [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '5_wteua^80be^ku_3h2#n33)i%2@)uu*lamy4(xsxb4^n*piy!'
 
 # Any host allowed
@@ -20,5 +21,3 @@ ALLOWED_HOSTS = ['*']
 CELERY_BROKER_URL = 'redis://redis:6379/1'
 CELERY_RESULT_BACKEND = 'redis://redis:6379/1'
 CELERY_TASK_ALWAYS_EAGER = False
-# Raise error for eager task
-CELERY_TASK_EAGER_PROPAGATES = True
